ca93fa2c5c2f3a7c366d9adaa9f7a88d8425ffa5
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2023-05-12 11:26:37 -0700

    external styles

---
+++ webstatic/styles/7-places.cs
@@ -0,0 +1,29 @@
+/* create global styles variable  */
+:root {
+    /* color variables */
+    --borderColor: #DDDDDD;
+    --lightRed: #FF5A5F;
+    --btnTxt: #FFFFFF;
+    --dropdownBg: #FAFAFA;
+}
+
+/* class tag styles  */
+.places > h1 {
+    font-size: 30px; 
+    margin-top: 20px;
+}
+
+.places > article {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 390px;
+    padding: 20px;
+    margin: 20px;
+    border: 1px solid var(--lightRed); 
+    border-radius: 4px;
+}
+
+.places > article > h2 {
+    font-size: 30px;
+}
