461bbac66a30239d87b714fd1beaff246dc83afd
Yaya <sikiruyaya1@gmail.com>
2023-05-15 10:06:23 -0700

    storage file constituting of all methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,34 +12,34 @@ from models.review import Review
 class FileStorage:
     ''' Defines the FileStorage Class '''
     __file_path = 'file.json'
-    __objects = {}
+    __objects = dict()
 
     def all(self):
-        ''' returns the dictionary of all created objects '''
+        ''' returns dictionary of all created objects '''
         return FileStorage.__objects
 
     def new(self, obj):
         '''
-            creates the dictionary of objects with
+            creates dictionary of objects with
             key as <class_name>.<obj_id> and
             value as the created object itself
          '''
         FileStorage.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
 
     def save(self):
-        '''  converts objects into JSON '''
+        ''' Serializes objects into JSON '''
         objects = FileStorage.__objects
         dict_from_obj = {key: obj.to_dict() for key, obj in objects.items()}
 
-        with open(FileStorage.__file_path, 'w') as file:
-            dump(dict_from_obj, file)
+        with open(FileStorage.__file_path, mode='w') as fd:
+            dump(dict_from_obj, fd)
 
     def reload(self):
-        ''' converts JSON into objects '''
+        ''' Deserializes JSON into objects '''
         try:
             with open(FileStorage.__file_path) as file:
                 dict_from_json = load(file)
                 for obj in dict_from_json.values():
                     self.new(eval(obj['__class__'])(**obj))
         except Exception:
-            return 'null'
+            return
